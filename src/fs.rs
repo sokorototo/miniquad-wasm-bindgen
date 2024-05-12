17090ce5b0fa67096217f49b57a156ef170ab421
sokorototo <nyachiengatoto@gmail.com>
2024-05-12 20:50:49 +0300

    slightly better XmlHttpRequest handling

--- src/fs.rs
+++ src/fs.rs
@@ -4,8 +4,9 @@ use crate::native::ios;
 #[derive(Debug)]
 pub enum Error {
 	IOError(std::io::Error),
-	/// XmlHttpRequest failed, contains status text
-	DownloadFailed(String),
+	/// XmlHttpRequest failed, returns [`ProgressEvent`](https://developer.mozilla.org/en-US/docs/Web/API/ProgressEvent) and Status Text
+	#[cfg(target_arch = "wasm32")]
+	DownloadFailed((web_sys::ProgressEvent, Option<String>)),
 	AndroidAssetLoadingError,
 	/// MainBundle pathForResource returned null
 	IOSAssetNoSuchFile,
@@ -74,29 +75,29 @@ mod wasm {
 	use super::{Error, Response};
 
 	use wasm_bindgen::{closure::Closure, JsCast, UnwrapThrowExt};
-	use web_sys::{js_sys, XmlHttpRequest};
+	use web_sys::*;
 
 	pub fn load_file<F: FnOnce(Response) + 'static>(path: &str, on_loaded: F) {
 		if let Ok(xhr) = XmlHttpRequest::new() {
 			if xhr.open("GET", path).is_ok() {
-				xhr.set_response_type(web_sys::XmlHttpRequestResponseType::Arraybuffer);
+				xhr.set_response_type(XmlHttpRequestResponseType::Arraybuffer);
 				xhr.set_timeout(5 * 1000); // 5 seconds
 
 				let xhr_1 = xhr.clone();
-				let present = Closure::once(move || {
+				let present = Closure::once(move |ev: ProgressEvent| {
 					match xhr_1.response() {
 						Ok(d) => {
 							if xhr_1.status().unwrap() != 200 {
 								#[cfg(feature = "log-impl")]
-								crate::error!("XmlHttpRequest failed: {:?}", xhr_1.status_text().unwrap());
-								on_loaded(Err(Error::DownloadFailed(xhr_1.status_text().unwrap_throw())));
+								crate::error!("XmlHttpRequest failed: {:?}", xhr_1.status_text());
+								on_loaded(Err(Error::DownloadFailed((ev, xhr_1.status_text().ok()))));
 							} else {
-								let array = d.dyn_into::<js_sys::ArrayBuffer>().unwrap();
+								let array = d.dyn_into::<js_sys::ArrayBuffer>().unwrap_throw();
 								let array = js_sys::Uint8Array::new(&array).to_vec();
 								on_loaded(Ok(array));
 							}
 						}
-						Err(e) => on_loaded(Err(Error::DownloadFailed(format!("XmlHttpRequest failed: {:?}", e)))),
+						Err(_) => on_loaded(Err(Error::DownloadFailed((ev, xhr_1.status_text().ok())))),
 					};
 				});
 
@@ -104,7 +105,8 @@ mod wasm {
 				xhr.set_onerror(Some(present.as_ref().unchecked_ref()));
 				xhr.set_onload(Some(present.as_ref().unchecked_ref()));
 			} else {
-				on_loaded(Err(Error::DownloadFailed("Unable to open XmlHttpRequest".to_string())));
+				let err = (ProgressEvent::new("Unable to Open XmlHttpRequest").unwrap(), None);
+				on_loaded(Err(Error::DownloadFailed(err)));
 			};
 
 			// Send the request
